9a6660b1e06d6aaffb435638f337a81b368cace4
selimgayretli <113355046+selimgayretli@users.noreply.github.com>
2024-11-01 13:44:19 +0300

    Adding the car parking system project

---
+++ Software/Activities/Car Parking System/Car-Parking-System-main.py
@@ -0,0 +1,46 @@
+#Car Parking System
+from microbit import *
+from picobricks import *
+
+motor = motordriver()
+motor.servo(1,90)
+
+while True:
+    if round(distance)<6:
+        motor.servo(1,180)
+        display.show(Image.YES)
+        sleep(1000)
+    else:
+        display.show(Image.NO)
+        motor.servo(1,180)
+        
+        
+        
+
+
+
+
+                    
+                    
+            
+            
+            
+            
+            
+        
+            
+        
+    
+ 
+    
+    
+        
+        
+        
+    
+        
+
+        
+
+
+
